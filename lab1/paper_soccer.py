9e4c20bc4f89b088985a32aa3bdccf0164d6ad41
s27081-pj <115358253+s27081@users.noreply.github.com>
2025-10-14 20:27:19 +0200

    Dodanie dokumentacji, zdjęcia rozgrywki i małe poprawki w kodzie
    * Dodana dokumentacja + drobne poprawki
    
    * Dodany screenshot rozgrywki
    
    ---------
    
    Co-authored-by: Ziemowit Orlikowski <ziemekorlikowski@gmail.com>

--- lab1/paper_soccer.py
+++ lab1/paper_soccer.py
@@ -1,29 +1,29 @@
-from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 import math
+
+from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 
 """
 Autorzy:
-- Patryk Kosmider
+- Patryk Kośmider
 - Ziemowit Orlikowski
 
 Cel:
-Celem gry jest doprowadzenie pilki na do bramki(x) przeciwnika.
-Gracz moze zwyciezyc na dwa sposoby:
+Celem gry jest doprowadzenie piłki do bramki (|x|) przeciwnika.
+Gracz może zwyciężyć na dwa sposoby:
 - strzelenie bramki przeciwnikowi
-- zablokowanie przeciwnika tak ze nie ma mozliwosci wykonania zadnego ruchu
+- zablokowanie przeciwnika tak, że nie ma on możliwości wykonania żadnego ruchu
 
 Zasady:
-- Kazdy gracz wykonuje jeden ruch na ture w dowolnym kierunku (lewy gorny, gorny, prawy gorny, lewo, prawo, lewy dolny, prawy dolny, dol)
-- Nie mozna stawac na polu na ktorym juz byla pilka
+- Każdy gracz wykonuje jeden ruch na turę w dowolnym kierunku (lewy górny, górny, prawy górny, lewy, prawy, lewy dolny, prawy dolny, dolny)
+- Nie można stawać na polu, na którym już była piłka.
 
 Przygotowanie do uruchomienia - wymagania:
 
-Srodowisko z Pythonem (Testowne na wersji 3.12)
+Środowisko z Pythonem
 Instalacja pakietu easyAI:
 
 - pip install easyAI
 """
-
 
 DIRECTIONS = {
     "q": (-1, -1),  # lewy górny
@@ -68,28 +68,34 @@ class PaperSoccer(TwoPlayerGame):
         self.blocked_position = []
 
     def is_over(self):
+        """
+        Sprawdza czy warunki zakonczenia gry zostały spełnione
+        :return: True/False
+        """
         if self.ball == self.goal_top:
+            print("Wygrał gracz!")
             return True
         if self.ball == self.goal_bottom:
+            print("Wygrało AI!")
             return True
         if not self.possible_moves():
+            print("Brak możliwych ruchów! Wygrał gracz", 3 - game.current_player, "!")
             return True
         return False
 
-    def make_move(self, move):
+    def make_move(self, character):
         """
         Wykonuje ruch piłką w wybranym kierunku, zapisuje ruch i sprawdza czy ruch jest dozwolony.
-        :param move: Kierunek ruchu (np. 'w', 'a', 'd', itp.)
+        :param character: Kierunek ruchu (np. 'w', 'a', 'd', itp.)
         :return: None
         """
-        if move not in DIRECTIONS:
+        if character not in DIRECTIONS:
             print(
                 "Nieprawidłowy kierunek ruchu. Dostępne ruchy:", list(DIRECTIONS.keys())
             )
-            self.show()
             return
-        dr, dc = DIRECTIONS[move]
-        move_char = CHARS_MAPPING[move]
+        dr, dc = DIRECTIONS[character]
+        move_char = CHARS_MAPPING[character]
         new_r = self.ball[0] + dr
         new_c = self.ball[1] + dc
         start = self.ball
@@ -101,18 +107,15 @@ class PaperSoccer(TwoPlayerGame):
         if not (0 <= new_r < self.rows and 0 <= new_c < self.cols):
             print("Ruch poza planszą! Nie możesz wykonać tego ruchu.")
             print("Możliwe ruchy:", self.possible_moves())
-            self.show()
             return
 
         if moving in self.moves or (end, start) in self.moves:
             print("Ten ruch już był! Nie możesz wykonać tej samej trasy.")
             print("Możliwe ruchy:", self.possible_moves())
-            self.show()
             return
 
         if (new_r == 0 or new_r == self.rows - 1) and new_c != self.cols // 2:
             print("Nie możesz wejść do bramki z boku! Tylko środek (x) jest otwarty.")
-            self.show()
             return
 
         self.moves.append(moving)
@@ -126,8 +129,8 @@ class PaperSoccer(TwoPlayerGame):
         Zwraca listę możliwych ruchów z aktualnej pozycji piłki.
         :return: Lista dozwolonych kierunków ruchu
         """
-        moves = []
-        for move, (dr, dc) in DIRECTIONS.items():
+        moves_list = []
+        for character, (dr, dc) in DIRECTIONS.items():
             new_r = self.ball[0] + dr
             new_c = self.ball[1] + dc
             end = (new_r, new_c)
@@ -142,20 +145,23 @@ class PaperSoccer(TwoPlayerGame):
             if end in self.blocked_position:
                 continue
 
-            moves.append(move)
-        return moves
+            moves_list.append(character)
+        return moves_list
 
     def scoring(self):
+        """
+        Logika punktacji dla AI, wyliczanie odległości piłki do obu bramek
+        :return: Różnica odległości piłki od dolnej bramki do górnej
+        """
         # pozycja piłki
         br, bc = self.ball
         # euklides do górnej
         dist_to_bottom_goal = math.sqrt(
-            (br - (self.goal_bottom[0] - 1)) ** 2
-            + (bc - (self.goal_bottom[1] // 2)) ** 2
+            (br - (self.goal_bottom[0])) ** 2 + (bc - (self.goal_bottom[1])) ** 2
         )
         # euklides do dolnej
         dist_to_top_goal = math.sqrt(
-            (br - (self.goal_top[0] + 1)) ** 2 + (bc - (self.goal_top[1] // 2)) ** 2
+            (br - (self.goal_top[0])) ** 2 + (bc - (self.goal_top[1])) ** 2
         )
         # Heurystyka:
         # Zwracamy różnicę: (odległość do dolnej) - (odległość do górnej)
@@ -166,15 +172,18 @@ class PaperSoccer(TwoPlayerGame):
         return dist_to_bottom_goal - dist_to_top_goal
 
     def show(self):
-        """ """
+        """
+        Generowanie planszy, rysowanie boiska, piłki oraz ruchów graczy
+        :return: None
+        """
         field_width = self.cols * 2 + 1
         pad = " " * 2
         goal = "|x|"
         goal_line = (
-            pad + "-" * (field_width // 2 - 1) + goal + "-" * (field_width // 2 - 1)
+                pad + "-" * (field_width // 2 - 1) + goal + "-" * (field_width // 2 - 1)
         )
         board = []
-        for i in range(self.rows):
+        for _ in range(self.rows):
             row = []
             for _ in range(self.cols):
                 row.append(".")
@@ -201,19 +210,13 @@ if __name__ == "__main__":
         # Zamiast game.play() robimy pętlę, żeby móc wyświetlać planszę i komunikaty
         game.show()
         moves = game.possible_moves()
-        if game.ball == game.goal_top:
-            print("Wygrał gracz!")
-            break
-        elif game.ball == game.goal_bottom:
-            print("Wygrało AI!")
-            break
-        elif not moves:
-            print("Brak możliwych ruchów! Wygrał gracz", 3 - game.current_player, "!")
-            break
         print("Możliwe ruchy:", moves)
         if game.current_player == 1:
-            move = input("Player 1 what do you play? ")
+            move = input("Jaki ruch wybierasz? ")
         else:
             move = game.players[1].ask_move(game)
             print(f"AI wybrało ruch: {move}")
         game.make_move(move)
+        if game.is_over():
+            game.show()
+            break
